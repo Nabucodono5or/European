cc4dcbd793f289442946f4a609a4861b32f867e4
Jefferson Abreu <nabucodonosor310@gmail.com>
2018-08-08 15:55:46 -0300

    adicionada as dependências do uibootstrap

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import appComponent from './app.component';
 import componentModule from './components/component';
+import ngAnimate from 'angular-animate';
+import angularTouch from 'angular-touch';
+import uiBootstrap from 'angular-ui-bootstrap';
 
-angular.module('app', [uiRouter, componentModule])
+angular.module('app', [uiRouter, componentModule, ngAnimate, uiBootstrap])
 
 .component('app', appComponent)
 
