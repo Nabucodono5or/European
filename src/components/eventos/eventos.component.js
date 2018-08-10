82c2d951da7f664e3a873a027592a85b7feb5094
Jefferson Abreu <pereirajefferson06@gmail.com>
2018-08-09 23:03:59 -0300

    criado componente eventos

--- src/components/eventos/eventos.component.js
+++ src/components/eventos/eventos.component.js
@@ -1,6 +1,9 @@
 import template from './eventos.html';
+import controller from './eventos.controller';
 
 let eventosComponent = {
+  bindings: {},
   template,
+  controller,
 }
 export default eventosComponent;
---
+++ src/components/eventos/eventos.controller.js
@@ -0,0 +1,38 @@
+class eventosController {
+  constructor(){
+    this.listaDeEventos = 
+    [ 
+      {   
+        nome: "Cooking Classes for All",
+        data: "February 28 - June 1 ",
+        endereco: " Galicia Restaurant, 234th Avenue New York, United States",
+        custo: "25.00",
+        desc: "Food is very important for every person. We must eat proteins, fats and so on. Food can have plant origin or animal origin. The first one consists of fruits and vegetables. Kids love fruits because they are sweet and delicious. But very often children don’t like vegetables, their parents are upset about it.",
+        imagem: "",
+      },  
+
+      {   
+        nome: "Let’s Try the Best Coffee",
+        data: "February 28 - June 1 ",
+        endereco: " Galicia Restaurant, 234th Avenue New York, United States",
+        custo: "25.00",
+        desc: "Food is very important for every person. We must eat proteins, fats and so on. Food can have plant origin or animal origin. The first one consists of fruits and vegetables. Kids love fruits because they are sweet and delicious. But very often children don’t like vegetables, their parents are upset about it.",
+        imagem: "",
+      },  
+
+      {   
+        nome: "Galicia’s Monday Night Supper",
+        data: "February 28 - June 1 ",
+        endereco: " Galicia Restaurant, 234th Avenue New York, United States",
+        custo: "25.00",
+        desc: "Food is very important for every person. We must eat proteins, fats and so on. Food can have plant origin or animal origin. The first one consists of fruits and vegetables. Kids love fruits because they are sweet and delicious. But very often children don’t like vegetables, their parents are upset about it.",
+        imagem: "",
+      },  
+
+    ];    
+  
+  }
+
+}
+
+export default eventosController;
