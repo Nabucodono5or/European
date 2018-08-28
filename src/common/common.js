fc13d5e6f730c3387bfef281cf77e46b81636627
Jefferson Abreu <nabucodonosor310@gmail.com>
2018-08-28 15:47:03 -0300

    criado component common e carregado corretamente

--- src/common/common.js
+++ src/common/common.js
@@ -1,9 +1,10 @@
 import angular from 'angular';
 import navbarModule from './navbar/navbar';
 import heroimageModule from './heroImage/heroimage';
+import footerModule from './footer/footer';
 
 
-let commonModule = angular.module('commonModule', [navbarModule, heroimageModule])
+let commonModule = angular.module('commonModule', [navbarModule, heroimageModule, footerModule])
 
 .name;
 
---
+++ src/common/footer/footer.component.js
@@ -0,0 +1,8 @@
+import template from './footer.html';
+
+let footerComponent = {
+  bindings: {},
+  template,
+}
+
+export default footerComponent;
