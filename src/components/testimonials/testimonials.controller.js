95954a052584b027322ab66f0b1daddec529852a
Jefferson Abreu <nabucodonosor310@gmail.com>
2018-08-28 14:26:40 -0300

    bug fix: concertado slides do carrousel

--- src/components/testimonials/testimonials.controller.js
+++ src/components/testimonials/testimonials.controller.js
@@ -1,23 +1,20 @@
 class testimonialsController {
   constructor(){
-    this.myInterval = 3000;
+    this.myInterval = 5000;
     this.slides = [
       {
-        id:1,
         image: "",
         testimonial: "Many desktop publishing packages and web pages editors now use Lorem Ipsum as their default model text, and search for 'lorem ipsum' will uncover many webstites still in their infancy",
         name: "Crunch Walnut",
         corporation: "Salmon & C. Cheese tempura=fried",
       },
       {
-        id:2,
         image: "",
         testimonial: "Many desktop publishing packages and web pages editors now use Lorem Ipsum as their default model text, and search for 'lorem ipsum' will uncover many webstites still in their infancy",
         name: "Crunch Walnut",
         corporation: "Salmon & C. Cheese tempura=fried",
       },
       {
-        id:3,
         image: "",
         testimonial: "Many desktop publishing packages and web pages editors now use Lorem Ipsum as their default model text, and search for 'lorem ipsum' will uncover many webstites still in their infancy",
         name: "Crunch Walnut",
