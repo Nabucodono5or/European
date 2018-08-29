f0b463a4655466eedfc56bb8eb3d57be3979ef8f
Jefferson Abreu <nabucodonosor310@gmail.com>
2018-08-29 15:43:31 -0300

    component google maps terminado

--- src/components/googleMap/googleMap.controller.js
+++ src/components/googleMap/googleMap.controller.js
@@ -1,7 +1,26 @@
+import angular from 'angular';
+
 class googleMapController {
-  constructor(){
-    
+  constructor(NgMap){
+    NgMap.getMap().then(function (map) {
+      console.log(map.getCenter());
+      console.log('markers', map.markers);
+      console.log('shapes', map.shapes);
+    })
   }
+
+/*
+myMap(){
+  let mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.12),
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.HYBRID,
+  };
+
+  let map = new google.maps.Map(angular.element('#map'), mapOptions);
+}
+
+*/
 }
 
 export default googleMapController;
--- src/components/googleMap/googleMap.js
+++ src/components/googleMap/googleMap.js
@@ -1,7 +1,8 @@
 import angular from 'angular';
+import ngMap from 'ngmap';
 import googleMapComponent from './googleMap.component';
 
-let googleMapModule = angular.module('googleMapModule', [])
+let googleMapModule = angular.module('googleMapModule', [ngMap])
 
   .component('googleMap', googleMapComponent)
 
