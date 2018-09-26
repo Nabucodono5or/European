5c3f15d4d809aee3f999b24a13862ddcddc4cc34
Jefferson Abreu <nabucodonosor310@gmail.com>
2018-09-26 15:52:29 -0300

    inserido as fotos no código do compoente lightbox

--- src/components/lightbox/lightbox.controller.js
+++ src/components/lightbox/lightbox.controller.js
@@ -1,6 +1,44 @@
+import photo1 from './images/adam-jaime-119568-unsplash.jpg';
+import photo2 from './images/benjamin-sow-774817-unsplash.jpg';
+import photo3 from './images/charles-koh-550122-unsplash.jpg';
+import photo4 from './images/christine-siracusa-363257-unsplash.jpg';
+import photo5 from './images/marie-france-latour-458412-unsplash.jpg';
+import photo6 from './images/rawpixel-256640-unsplash.jpg';
+
 class lightboxController {
   constructor(){
+    this.exibir = false;
+    this.currentIndex = 1;
+    this.photos = [
+      photo1,
+      photo2,
+      photo3,
+      photo4,
+      photo5,
+      photo6
+    ];
+  }
 
+
+  clickOpenImg(index){
+
+    this.currentIndex = index;
+
+    if(index > (this.photos.length-1)) { this.currentIndex = 0 };
+    if(index < 0) { this.currentIndex = this.photos.length-1 };
+
+    this.exibir = true;
+    this.imageSrc = this.photos[this.currentIndex];
+  }
+
+
+  clickCloseImg(){
+    this.exibir = false;
+  }
+
+
+  plusSlides(n){
+    this.clickOpenImg(this.currentIndex += n);
   }
 
 }
