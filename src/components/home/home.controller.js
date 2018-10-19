28a0ff9192b261eb3c3fe4a9d4bcd23db47ebdc4
Jefferson Abreu <pereirajefferson06@gmail.com>
2018-10-18 22:16:08 -0300

    criado componente home com template completo

---
+++ src/components/home/home.controller.js
@@ -0,0 +1,72 @@
+class homeController {
+  constructor(){
+    this.slides = [
+      {
+        image: "",
+        testimonial: "Many desktop publishing packages and web pages editors now use Lorem Ipsum as their default model text, and search for 'lorem ipsum' will uncover many webstites still in their infancy",
+        name: "Crunch Walnut",
+        corporation: "Salmon & C. Cheese tempura=fried",
+      }
+    ]
+
+    this.ofertas = [
+      {
+        image: "",
+        name: "California with sesame",
+        ingredientes: "Cream cheese, avocado, cucumber, salmon, sushi rice, nori, pickled ginger, wasab",
+        preco: "34.00",
+      },
+
+      {
+        image: "",
+        name: "Philadelphia with shrimp",
+        ingredientes: "Cream cheese, avocado, cucumber, salmon, sushi rice, nori, pickled ginger, wasab",
+        preco: "34.00",       
+      },
+
+      {
+        image: "",
+        name: "Roll with tuna",
+        ingredientes: "Cream cheese, avocado, cucumber, salmon, sushi rice, nori, pickled ginger, wasab",
+        preco: "34.00",
+      },
+
+      {
+        image: "",
+        name: "California with sesame",
+        ingredientes: "Cream cheese, avocado, cucumber, salmon, sushi rice, nori, pickled ginger, wasab",
+        preco: "34.00",
+      },
+
+      {
+        image: "",
+        name: "Philadelphia with salmon",
+        ingredientes: "Cream cheese, avocado, cucumber, salmon, sushi rice, nori, pickled ginger, wasab",
+        preco: "34.00",         
+      },
+
+      {
+        image: "",
+        name: "California with sesame",
+        ingredientes: "Cream cheese, avocado, cucumber, salmon, sushi rice, nori, pickled ginger, wasab",
+        preco: "34.00",        
+      },
+
+      {
+        image: "",
+        name: "Smile Roll",
+        ingredientes: "Cream cheese, avocado, cucumber, salmon, sushi rice, nori, pickled ginger, wasab",
+        preco: "34.00",
+      },
+
+      {
+        image: "",
+        name: "Smoked salmon crunch",
+        ingredientes: "Cream cheese, avocado, cucumber, salmon, sushi rice, nori, pickled ginger, wasab",
+        preco: "34.00",        
+      }
+    ]
+  }
+}
+
+export default homeController;
